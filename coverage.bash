3f2868aec99406611c5b5b6abc65fe04651a2c5b
myint <myint@macbookpro.local>
2011-12-03 09:50:48 -0800

    Add lines to ignore coverage files.

--- coverage.bash
+++ coverage.bash
@@ -17,4 +17,7 @@ lcov --directory='CMakeFiles/perceptualdiff.dir' --output-file='lcov_tmp.info' -
 lcov --output-file='lcov.info' --extract 'lcov_tmp.info' "$(pwd)/*"
 genhtml --output-directory='coverage_output' lcov.info
 
+# Remove intermediate lcov output
+rm 'lcov_tmp.info'
+
 echo -e '\nCoverage report: coverage_output/index.html'
