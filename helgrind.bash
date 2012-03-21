82913092f743bf52ce6601c08a7b09d3eb70a2cb
myint <myint@macbookpro.local>
2012-03-21 00:17:23 -0700

    Disable helgrind as it generates false postives

--- helgrind.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Run Valgrind helgrind on perceptualdiff.
-
-valgrind --error-exitcode=123 --quiet --tool=helgrind --gen-suppressions=all ./perceptualdiff test/cam_mb.tif test/cam_mb_ref.tif
