599dfb7600986d6fb5a6b8bd72640d8d3fa6e2c0
Steven Myint <git@stevenmyint.com>
2013-04-19 09:07:16 -0700

    Properly accumulate before copying results

--- coveralls.bash
+++ coveralls.bash
@@ -5,11 +5,19 @@
 ./coverage.bash
 
 path=$PWD
+result_path="$path/CMakeFiles/perceptualdiff.dir"
+
 for f in *.cpp *.h
 do
     base=$(basename "$f" .cpp)
     base=$(basename "$base" .h)
+    cd "$result_path"
     gcov "$base"
-    cd "$path/CMakeFiles/perceptualdiff.dir"
-    cp "$f.gcov" "$path" || true
+done
+
+# Do this in a separate pass to make sure everything is accumulated.
+cd "$path"
+for f in *.cpp *.h
+do
+    cp "$result_path/$f.gcov" "$path" || true
 done
