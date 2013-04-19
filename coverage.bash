7ee4aae8851e16d02a1cd392a149b14f30e30918
Steven Myint <git@stevenmyint.com>
2013-04-18 19:46:36 -0700

    Copy over gcov files

--- coverage.bash
+++ coverage.bash
@@ -21,3 +21,14 @@ genhtml --output-directory='coverage_output' lcov.info
 rm 'lcov_tmp.info'
 
 echo -e '\nCoverage report: coverage_output/index.html'
+
+# For Coveralls.
+path=$PWD
+for f in *.cpp *.h
+do
+    base=$(basename "$f" .cpp)
+    base=$(basename "$base" .h)
+    gcov "$base"
+    cd "$path/CMakeFiles/perceptualdiff.dir"
+    cp "$f.gcov" "$path"
+done
