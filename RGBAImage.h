f281e867a262ed12e1f7014943592b70ef9765ba
Steven Myint <git@stevenmyint.com>
2013-08-09 19:23:55 -0700

    Fix header guard

--- RGBAImage.h
+++ RGBAImage.h
@@ -16,7 +16,7 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#ifndef _RGAIMAGE_H
+#ifndef _RGBAIMAGE_H
 #define _RGBAIMAGE_H
 
 #include <string>
