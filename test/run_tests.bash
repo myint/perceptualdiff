43ee0aacaeb7d4af2b94f3d236271393fb5872f7
Steven Myint <git@stevenmyint.com>
2013-09-03 20:06:49 -0700

    Test invalid arguments

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -79,5 +79,6 @@ $pdiff  /dev/null /dev/null | grep -q 'FAIL'
 $pdiff -verbose -sum-errors fish[12].png | grep -q 'sum'
 $pdiff -colorfactor .5 -threshold 1000 -gamma 3 -luminance 90 cam_mb_ref.tif cam_mb.tif
 $pdiff -verbose -downsample 30 -scale -luminanceonly -fov 80 cam_mb_ref.tif cam_mb.tif
+echo "$($pdiff -fov wrong fish1.png fish1.png 2>&1)" | grep -q 'Invalid argument'
 
 echo -e '\x1b[01;32mOK\x1b[0m'
