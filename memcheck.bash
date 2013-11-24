51b43a56c74fed79e4e991a98679ee273e59124b
Steven Myint <git@stevenmyint.com>
2013-11-24 08:50:01 -0800

    Generalize

--- memcheck.bash
+++ memcheck.bash
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -ex
 #
 # Run Valgrind memcheck on perceptualdiff.
 
-valgrind --error-exitcode=123 --quiet --leak-check=full --gen-suppressions=all --suppressions=memcheck.supp ./perceptualdiff test/cam_mb.tif test/cam_mb_ref.tif
+valgrind --error-exitcode=2 --quiet --leak-check=full --gen-suppressions=all --suppressions=memcheck.supp ./perceptualdiff test/cam_mb.tif test/cam_mb_ref.tif
