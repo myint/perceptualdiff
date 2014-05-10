69362484d38feed00ad6c74fb14f2f0f845f831a
Steven Myint <git@stevenmyint.com>
2014-05-10 08:44:33 -0700

    Use specific g++ for Coverity

--- coverity.bash
+++ coverity.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
 rm -rf perceptualdiff.tgz cov-int
-cov-build --dir cov-int g++ -std=c++0x -o perceptualdiff ./*.cpp -lfreeimage
+cov-build --dir cov-int g++-mp-4.8 -std=c++0x -o perceptualdiff ./*.cpp -lfreeimage
 tar czvf perceptualdiff.tgz cov-int
