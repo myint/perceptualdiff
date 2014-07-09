efcffe9c43366085084f8a6705908077597997dd
Steven Myint <git@stevenmyint.com>
2014-07-09 08:12:01 -0700

    Modify for coverage

--- RGBAImage.cpp
+++ RGBAImage.cpp
@@ -29,7 +29,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 
 struct FreeImageDeleter
 {
-    void operator()(FIBITMAP *image)
+    inline void operator()(FIBITMAP *image)
     {
         if (image)
         {
