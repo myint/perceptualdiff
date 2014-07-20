7311b3bacdf47b12d30ead42c95a6fa3b7981b4f
Steven Myint <git@stevenmyint.com>
2014-07-20 06:18:52 -0700

    Format

--- compare_args.h
+++ compare_args.h
@@ -24,7 +24,9 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 #include <stdexcept>
 #include <string>
 
+
 class RGBAImage;
+
 
 // Arguments to pass into the comparison function.
 class CompareArgs
