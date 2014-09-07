129633525c7bc6f5eaa148c7c43f39a70ebac9d1
Steven Myint <git@stevenmyint.com>
2014-09-07 08:12:05 -0700

    Document

--- coverity.bash
+++ coverity.bash
@@ -1,5 +1,10 @@
 #!/bin/bash -eux
 
+# Build Coverity intermediate files.
+
+# This assumes Coverity has already been configured via
+# "cov-configure --compiler g++-mp-4.8".
+
 rm -rf perceptualdiff.tgz cov-int
 cov-build --dir cov-int g++-mp-4.8 -std=c++0x -o perceptualdiff ./*.cpp -lfreeimage
 tar czvf perceptualdiff.tgz cov-int
