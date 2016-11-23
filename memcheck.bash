7b5aea14fbb95433bf3cb22731fcbabf32b995ba
Steven Myint <git@stevenmyint.com>
2016-11-23 09:58:02 -0800

    Use build directory

--- memcheck.bash
+++ memcheck.bash
@@ -1,11 +1,20 @@
-#!/bin/bash -ex
+#!/bin/bash
 #
 # Run Valgrind memcheck on perceptualdiff.
+
+set -eux
 
 VALGRIND="valgrind --error-exitcode=2 --quiet --leak-check=full \
     --track-origins=yes --gen-suppressions=all --suppressions=memcheck.supp"
 
-$VALGRIND ./perceptualdiff test/cam_mb.tif test/cam_mb_ref.tif
+for d in ./build .
+do
+    pdiff="$d/perceptualdiff"
+    if [ -f "$pdiff" ]; then
+        break
+    fi
+done
 
-$VALGRIND ./perceptualdiff --verbose --downsample 9 \
-    test/fish1.png test/fish2.png
+$VALGRIND "$pdiff" test/cam_mb.tif test/cam_mb_ref.tif
+
+$VALGRIND "$pdiff" --verbose --downsample 9 test/fish1.png test/fish2.png
